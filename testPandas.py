7e1e9fce8b131b75964937bad7c58c11acfe9583
dwaskevich <d.waskevich@comcast.net>
2024-06-30 22:17:53 -0500

    added capability to navigate/process additional pages

--- testPandas.py
+++ testPandas.py
@@ -28,6 +28,8 @@ soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
 # variable to hold repo url
 repositoryURL = ''
 
+numRepos = 0  # variable to count number of repositories (also used as data frame index)
+
 # find repo page url
 for tag in soup.find_all('a', {'data-tab-item': 'repositories'}):
     repositoryURL = tag.get('href')
@@ -37,39 +39,47 @@ url = baseURL.removesuffix('/') + repositoryURL
 
 print('Repositories url link: ', url, end='\n\n')  # put link in console
 
-# Send a new GET request to repositories url
-response = requests.get(url)
-
-html_doc = response.text  # extract html contents
-soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
-
-numRepos = 0  # variable to count number of repositories (also used as data frame index)
-
 # instantiate a Pandas object
 summaryTable = pd.DataFrame(columns=columnHeadings)
 
-# find all user repositories and extract summary details (name, url, language, description)
-for tag in soup.find_all('li', {'itemprop': "owns"}):
-    repoName = tag.find('a')
-    repoName = repoName.string.lstrip()
-    repoURL = tag.find('a', {'itemprop': 'name codeRepository'})
-    repoURL = baseURL.removesuffix('/') + repoURL.get('href')
-    repoLanguage = tag.find('span', {'itemprop': 'programmingLanguage'})
-    repoLanguage = repoLanguage.string.lstrip()
-    repoDescription = tag.find('p', {'itemprop': 'description'})
-    if repoDescription is not None:
-        repoDescription = repoDescription.string.lstrip()
-        repoDescription = repoDescription.rstrip()
+while True:
+    # Send a new GET request to repositories url
+    response = requests.get(url)
 
-    # output to console
-    print(repoName, repoURL, repoLanguage, repoDescription)
+    html_doc = response.text  # extract html contents
+    soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
 
-    # populate data frame
-    summaryTable.at[numRepos, 'Repo'] = repoName
-    summaryTable.at[numRepos, 'URL'] = repoURL
-    summaryTable.at[numRepos, 'Language'] = repoLanguage
-    summaryTable.at[numRepos, 'Description'] = repoDescription
-    numRepos += 1  # count number of repositories found
+    # find all user repositories and extract summary details (name, url, language, description)
+    for tag in soup.find_all('li', {'itemprop': "owns"}):
+        repoName = tag.find('a')
+        repoName = repoName.string.lstrip()
+        repoURL = tag.find('a', {'itemprop': 'name codeRepository'})
+        repoURL = baseURL.removesuffix('/') + repoURL.get('href')
+        repoLanguage = tag.find('span', {'itemprop': 'programmingLanguage'})
+        repoLanguage = repoLanguage.string.lstrip()
+        repoDescription = tag.find('p', {'itemprop': 'description'})
+        if repoDescription is not None:
+            repoDescription = repoDescription.string.lstrip()
+            repoDescription = repoDescription.rstrip()
+
+        # output to console
+        print(repoName, repoURL, repoLanguage, repoDescription)
+
+        # populate data frame
+        summaryTable.at[numRepos, 'Repo'] = repoName
+        summaryTable.at[numRepos, 'URL'] = repoURL
+        summaryTable.at[numRepos, 'Language'] = repoLanguage
+        summaryTable.at[numRepos, 'Description'] = repoDescription
+        numRepos += 1  # count number of repositories found
+
+    # check for another page of repositories
+    nextPage = soup.find('a', class_='next_page')
+
+    if nextPage:
+        # construct url for next page and continue in while loop
+        url = baseURL.removesuffix('/') + nextPage.get('href')
+    else:
+        break
 
 print(f'\nNumber of repositories found = {numRepos}\n')
 
