a5337e1f86f7aaa2cdbbdf3dc6430d1468a7bcc7
David Waskevich <dwaskevich@outlook.com>
2024-09-20 21:28:52 -0500

    added "while True" loop to continue requesting user input

--- testPandas.py
+++ testPandas.py
@@ -5,88 +5,89 @@ import pandas as pd
 baseURL = 'https://github.com/'  # Github domain
 columnHeadings = ['Repo', 'URL', 'Language', 'Description']  # define data frame column headings
 
-# get user input
 while True:
-    userName = input("Enter Github user name: ")
+    # get user input
+    while True:
+        userName = input("Enter Github user name: ")
 
-    url = baseURL + userName  # generate full url
-    print(url, end='\n\n')
+        url = baseURL + userName  # generate full url
+        print(url, end='\n\n')
 
-    # Send a GET request to the full/generated url (i.e. user's github landing page)
-    response = requests.get(url)
+        # Send a GET request to the full/generated url (i.e. user's github landing page)
+        response = requests.get(url)
 
-    # check for server response
-    if response.status_code == requests.codes.ok:
-        break
-    else:
-        print('No response from ' + url)
+        # check for server response
+        if response.status_code == requests.codes.ok:
+            break
+        else:
+            print('No response from ' + url)
 
-# process server response
-html_doc = response.text  # extract html contents
-soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
-
-# variable to hold repo url
-repositoryURL = ''
-
-numRepos = 0  # variable to count number of repositories (also used as data frame index)
-
-# find repo page url
-for tag in soup.find_all('a', {'data-tab-item': 'repositories'}):
-    repositoryURL = tag.get('href')
-
-# construct repositories url (remove trailing '/' on base URL to avoid double slash)
-url = baseURL.removesuffix('/') + repositoryURL
-
-print('Repositories url link: ', url, end='\n\n')  # put link in console
-
-# instantiate a Pandas object
-summaryTable = pd.DataFrame(columns=columnHeadings)
-
-while True:
-    # Send a new GET request to repositories url
-    response = requests.get(url)
-
+    # process server response
     html_doc = response.text  # extract html contents
     soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
 
-    # find all user repositories and extract summary details (name, url, language, description)
-    for tag in soup.find_all('li', {'itemprop': "owns"}):
-        repoName = tag.find('a')
-        repoName = repoName.string.lstrip()
-        repoURL = tag.find('a', {'itemprop': 'name codeRepository'})
-        repoURL = baseURL.removesuffix('/') + repoURL.get('href')
-        repoLanguage = tag.find('span', {'itemprop': 'programmingLanguage'})
-        if repoLanguage is not None:
-            repoLanguage = repoLanguage.string.lstrip()
-        repoDescription = tag.find('p', {'itemprop': 'description'})
-        if repoDescription is not None:
-            repoDescription = repoDescription.string.lstrip()
-            repoDescription = repoDescription.rstrip()
+    # variable to hold repo url
+    repositoryURL = ''
 
-        # output to console
-        print(repoName, repoURL, repoLanguage, repoDescription)
+    numRepos = 0  # variable to count number of repositories (also used as data frame index)
 
-        # populate data frame
-        summaryTable.at[numRepos, 'Repo'] = repoName
-        summaryTable.at[numRepos, 'URL'] = repoURL
-        summaryTable.at[numRepos, 'Language'] = repoLanguage
-        summaryTable.at[numRepos, 'Description'] = repoDescription
-        numRepos += 1  # count number of repositories found
+    # find repo page url
+    for tag in soup.find_all('a', {'data-tab-item': 'repositories'}):
+        repositoryURL = tag.get('href')
 
-    # check for another page of repositories
-    nextPage = soup.find('a', class_='next_page')
+    # construct repositories url (remove trailing '/' on base URL to avoid double slash)
+    url = baseURL.removesuffix('/') + repositoryURL
 
-    if nextPage:
-        # construct url for next page and continue in while loop
-        url = baseURL.removesuffix('/') + nextPage.get('href')
-    else:
-        break
+    print('Repositories url link: ', url, end='\n\n')  # put link in console
 
-print(f'\nNumber of repositories found = {numRepos}\n')
+    # instantiate a Pandas object
+    summaryTable = pd.DataFrame(columns=columnHeadings)
 
-print(summaryTable)
+    while True:
+        # Send a new GET request to repositories url
+        response = requests.get(url)
 
-# export captured data to .csv file
-summaryTable.to_csv(userName + '.csv', index=False)
+        html_doc = response.text  # extract html contents
+        soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
 
-print(f'\nWrote data to file: {userName + '.csv'}.')
+        # find all user repositories and extract summary details (name, url, language, description)
+        for tag in soup.find_all('li', {'itemprop': "owns"}):
+            repoName = tag.find('a')
+            repoName = repoName.string.lstrip()
+            repoURL = tag.find('a', {'itemprop': 'name codeRepository'})
+            repoURL = baseURL.removesuffix('/') + repoURL.get('href')
+            repoLanguage = tag.find('span', {'itemprop': 'programmingLanguage'})
+            if repoLanguage is not None:
+                repoLanguage = repoLanguage.string.lstrip()
+            repoDescription = tag.find('p', {'itemprop': 'description'})
+            if repoDescription is not None:
+                repoDescription = repoDescription.string.lstrip()
+                repoDescription = repoDescription.rstrip()
+
+            # output to console
+            print(repoName, repoURL, repoLanguage, repoDescription)
+
+            # populate data frame
+            summaryTable.at[numRepos, 'Repo'] = repoName
+            summaryTable.at[numRepos, 'URL'] = repoURL
+            summaryTable.at[numRepos, 'Language'] = repoLanguage
+            summaryTable.at[numRepos, 'Description'] = repoDescription
+            numRepos += 1  # count number of repositories found
+
+        # check for another page of repositories
+        nextPage = soup.find('a', class_='next_page')
+
+        if nextPage:
+            # construct url for next page and continue in while loop
+            url = baseURL.removesuffix('/') + nextPage.get('href')
+        else:
+            break
+
+    print(f'\nNumber of repositories found = {numRepos}\n')
+
+    print(summaryTable)
+
+    # export captured data to .csv file
+    summaryTable.to_csv(userName + '.csv', index=False)
+
+    print(f'\nWrote data to file: {userName + '.csv'}.')
